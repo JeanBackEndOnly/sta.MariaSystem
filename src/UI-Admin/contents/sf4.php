388c2509020fcf1f78d5e490f349551b4a67aa3e
JeanBackEndOnly <145352326+JeanBackEndOnly@users.noreply.github.com>
2025-09-27 11:18:03 +0800

    asd
    asd

---
+++ src/UI-Admin/contents/sf4.php
@@ -0,0 +1,20 @@
+<main>
+    <div class="col-md-12 d-flex flex-column align-items-center justify-content-center">
+         <strong class="text-dark fs-4">School Form 4 (SF4) Monthly Learner's Movement and Attendance</strong>
+        <p  class="text-dark">(this replaces Form 3 & STS Form 4-Absenteeism and Dropout Profile)</p>
+    </div>
+    <div class="col-md-12 d-flex m-0 p-0">
+        <span class="me-4">School ID <input style="margin-left: 26px" type="text" name="schoolID"> <input type="text" name="region"></span>
+        <span class="me-4">Division <input style="margin-left: 26px" type="text" name="division"></span>
+        <span style="transform: translateX(65px);">District <input type="text" style="width: 9.5rem;" name="district"></span>
+    </div>
+    <div class="col-md-12 d-flex m-0 p-0">
+        <span class="me-4">School Name <input type="text" name="schoolName" style="width: 25.5rem;"></span>
+        <span class="me-4">School Year <input style="width: 9.5rem;" type="text" name="division"></span>
+        <span>Report for the month of <input style="width: 9.5rem;" type="text" name="district"></span>
+    </div>
+    <div class="col-md-12">
+        
+    </div>
+   
+</main>
