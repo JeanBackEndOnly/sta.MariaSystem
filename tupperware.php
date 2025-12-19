1ba0c79310a0e625f4cc2ed01fc5e976e2583400
Hikusama <leomessi25k@gmail.com>
2025-12-19 23:51:40 +0800

    BASE_FR added for script base path

--- tupperware.php
+++ tupperware.php
@@ -7,7 +7,7 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 define('BASE_PATH', __DIR__);
 // define('BASE_PATH', __DIR__);
-
+define('BASE_FR', '/sta.MariaSystem');
 // Composer autoload (relative to this file)
 require_once __DIR__ . '/authentication/functions.php';
 require_once __DIR__ . '/vendor/autoload.php';
