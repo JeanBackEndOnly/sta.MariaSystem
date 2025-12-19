24048f86953cc257d9bfac157427b6963342beca
Hikusama <leomessi25k@gmail.com>
2025-12-20 02:54:21 +0800

    for testing

--- header.php
+++ header.php
@@ -9,6 +9,7 @@ include 'authentication/session.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="<?php echo get_option('system_description')?>">
     <title>sta.Maria web system</title>
+    <link rel="icon" href="<?php echo base_url() ?>/assets/image/logo2.png" type="image/x-icon">
     <?php render_styles()?>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
