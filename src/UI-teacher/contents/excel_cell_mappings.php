28ca3266e7f4927dd82e341d2f6417d24564230b
hikusama <leomessi25k@gmail.com>
2026-01-21 16:04:15 +0800

    fixed cell position
    Co-authored-by: hikusama <leomessi25k@gmail.com>

---
+++ src/UI-teacher/contents/excel_cell_mappings.php
@@ -0,0 +1,46 @@
+<?php
+
+const EXCEL_CELL_MAPPINGS = [
+    'lastname' => 'c9',
+    'firstname' => 'h9',
+    'suffix' => 'n9',
+    'mname' => 's9',
+    'lrn' => 'e10',
+    'bd' => 'l10',
+    'sx' => 't10',
+    'kinderprogress' => 'f14',
+    'eccd' => 'l14',
+    'kinder_cert' => 'q14',
+    'nos' => 'd15',
+    'schoolid' => 'k15',
+    'aos' => 'q15',
+    'pept_passer' => 'b19',
+    'doe' => 'k19',
+    'otherscheckbx' => 'p19',
+    'othersinput' => 't19',
+    'natC' => 'f20',
+    'remark' => 's20'
+];
+
+const CELL_TO_FIELD_MAPPINGS = [
+    'c9' => 'lastname',
+    'h9' => 'firstname',
+    'n9' => 'suffix',
+    's9' => 'mname',
+    'e10' => 'lrn',
+    'l10' => 'bd',
+    't10' => 'sx',
+    'f14' => 'kinderprogress',
+    'l14' => 'eccd',
+    'q14' => 'kinder_cert',
+    'd15' => 'nos',
+    'k15' => 'schoolid',
+    'q15' => 'aos',
+    'b19' => 'pept_passer',
+    'k19' => 'doe',
+    'p19' => 'otherscheckbx',
+    't19' => 'othersinput',
+    'f20' => 'natC',
+    's20' => 'remark'
+];
+?>
